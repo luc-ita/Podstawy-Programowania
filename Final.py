2acc070e78d53c15e554c5c902eaeb97e82ed54a
luc-ita <lucaz5@vp.pl>
2024-10-27 15:59:47 +0100

    Create Final.py

---
+++ Final.py
@@ -0,0 +1,151 @@
+###
+# 3.1
+#
+company = "ABC Data"
+phone = "555-123-009"
+employees = 25
+remote_work = True
+big_company = employees > 100
+income = 4500000
+income_per_person = income / employees
+
+print("Variable `company` has value: ", company)
+print("and is", type(company))
+print("Variable `phone` has value: ", phone)
+print("and is", type(phone))
+print("Variable `employees` has value: ", employees)
+print("and is", type(employees))
+print("Variable `remote_work` has value: ", remote_work)
+print("and is", type(remote_work))
+print("Variable `big_company` has value: ", big_company)
+print("and is", type(big_company))
+print("Variable `income` has value: ", income)
+print("and is", type(income))
+print("Variable `income_per_person` has value: ", income_per_person)
+print("and is", type(income_per_person))
+
+
+###
+# 3.2
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 71
+number2 = 14
+number3 = 25
+result = number1 + number2 + number3
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('Number 3: ', number3)
+print('The result of summation: ', result)
+
+
+###
+# 3.3
+# A program for swapping two varable values
+#
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+
+# swap the values
+z = x
+x = y
+y = z
+
+print("After swapping: x=", x, "y=", y)
+
+
+###
+# 3.4
+# A program that, for a given speed in km/h,
+# prints the speed in m/s
+speed_kmh = 70
+speed_ms = speed_kmh * 1000 / 3600
+print(speed_kmh, "km/h = ", speed_ms, "m/s")
+
+###
+# 3.5
+# A program that calculates the length of the diagonal
+# of a rectangle with sides a and b.
+#
+import math
+a = 5
+b = 8
+diagonal = math.sqrt(a**2 + b**2)
+print("We have rectangle with sides ", a, " and ", b)
+print("Diagonal is ", diagonal)
+
+
+###
+# 3.6
+# Program that calculates the distance 
+# to the horizon from a height given
+import math
+
+height = input("Enter your height in m: ")
+height = float(height)
+d = 3.57 * math.sqrt(height)
+print("Distance you see on a beach is: ", d, "km")
+height = height + 20
+d = 3.57 * math.sqrt(height)
+print("Distance you see from hotel is: ", d, "km")
+
+
+###
+# 3.7
+# A program that calculates and prints:
+# - the number of people and percentage of the total
+#   population living in the Northern Hemisphere
+# - the number of people and percentage of the total
+#   population living in the Southern Hemisphere
+#
+total = 8000000000
+north = 7200000000
+south = total - north
+print("World population: ", total)
+print("Northern Hemisphere: ", north)
+print("Northern Hemisphere in %: ", north/total*100)
+print("Southern Hemisphere: ", south)
+print("Nouthern Hemisphere in %: ", south/total*100)
+
+
+###
+# 3.8
+# A program that calculates and prints
+# the average grade of a student
+#
+math = 5
+art = 4
+music = 5
+history = 3
+average = (math + art + music + history) / 4
+print("Average grade is", average)
+
+
+###
+# 4.1
+# Printing student's personal data
+#
+name = "Adam"
+age = 24
+height = 180
+future_age = age + 6
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {future_age} years old.  ")
+
+
+###
+# 4.2
+# Write a program that calculates and prints
+# the income of a family per person. To print the results
+# in a readable form, use f-strings.
+#
+father_income = 5450
+mother_income = 4920
+family_members = 5
+total_income = father_income + mother_income
+income_per_person = total_income / family_members
+print(f'Total family income is {total_income}, and income per person is {income_per_person}')
