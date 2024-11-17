f47a65959ff73be2dcb57a968fe0a0ba3173fe89
luc-ita <lucaz5@vp.pl>
2024-11-17 07:35:12 +0100

    7.9

--- Final.py
+++ Final.py
@@ -351,3 +351,103 @@ if (movie.find("dragon")) == -1:
     print('There is no dragon in this title')
 else:
     print('dragon is on', movie.find("dragon"))
+
+###
+# 7.1
+# People up to and including 26 years of age are exempt
+# from paying taxes in Poland. Write a program that,
+# based on the person's age entered from the keyboard,
+# prints True if the person is exempt from paying taxes
+# and prints False otherwise.
+#
+age = int(input('Enter age: '))
+no_tax = age < 27
+print(f'Exemption from paying taxes: {no_tax}')
+
+###
+# 7.2
+# A program that checks whether the password length
+# read from the keyboard is correct.
+#
+password = input('Enter password: ')
+password_ok = len(password) >= 8
+print(f'Password length is valid: {password_ok}')
+
+###
+# 7.3
+# A program that checks whether the number entered
+# from the keyboard is even.
+# A number is even if the remainder when divided by 2 is 0.
+# What operator do you need to use to calculate
+# the remainder of division?
+#
+number = int(input('Enter number: '))
+modulo = number % 2
+print(modulo == 0)
+
+###
+# 7.4
+# A program that , based on the circumference of the tree 
+# entered from the keyboard, calculates and prints 
+# the value True if the tree can be cut down, or print 
+# the value False otherwise. 
+#
+c = int(input('Enter tree circumference in cm: '))
+d = c/3.14
+print(d <= 50)
+
+###
+# 7.5
+# Vehicle registration numbers in Krakow start
+# with the letters KR or KK. Write a program that checks
+# whether the vehicle registration number entered
+# from the keyboard means a vehicle from Krakow.
+# Print True whether a car is from Krakow or False otherwise.
+#
+car_number = input('Enter car registration number: ')
+car_number = car_number.upper()
+is_krakow = car_number[0:2] == "KR" or car_number[0:2] == "KK"
+print(f'Car is from Krakow: {is_krakow}')
+
+###
+# 7.6
+# A program that checks whether the vehicle speed 
+# entered from the keyboard is correct.
+#
+s = int(input('Enter vehicle speed: '))
+print(s > 40 and s < 140)
+
+###
+# 7.7
+# A program that generate random number in the range(5,10).
+#
+import random
+random_number = random.randint(5,10)
+print(random_number)
+
+###
+# 7.8
+# A program that prints results of three dice rolls
+# and the sum of dice rolled.
+#
+import random
+dice_roll_1 = random.randint(1,6)
+dice_roll_2 = random.randint(1,6)
+dice_roll_3 = random.randint(1,6)
+total = dice_roll_1 + dice_roll_2 + dice_roll_3
+print('Sum of dice rolled:', total)
+
+###
+# 7.9
+# A program that prints the number of dice rolled and 
+# the value True if the number rolled is 1 or 6.
+#
+import random
+d1 = random.randint(1,6)
+d2 = random.randint(1,6)
+d3 = random.randint(1,6)
+d4 = random.randint(1,6)
+print(d1, d2, d3, d4)
+print()
+print('Dice rolled: 4')
+print(f'Special number (1 or 6): {d1 == 1 or d1 == 6 or d2 == 1 or d2 == 6 or d3 == 1 or d3 == 6 or d4 == 1 or d4 == 6}')
--- WorkInProgres.py
+++ WorkInProgres.py
@@ -1,27 +1,14 @@
 ###
-# 6.10
-# String manipulation
+# 7.8
+# A program that prints the number of dice rolled and 
+# the value True if the number rolled is 1 or 6.
 #
-
-movie = "The Lord of the Rings: The Return of the King"
-
-# print number of characters
-print('Number of characters: ', len(movie))
-
-# print title in capital letters
-print(movie.upper())
-
-# print title in small letters
-print(movie.casefold())
-
-# print how many times the vowel "e" appears in the title
-print(movie.count("e"), ' of e')
-
-# print where in the text is the word "Lord"
-print('Lord is on', movie.find("Lord"))
-
-# print where in the text is the word "dragon"
-if (movie.find("dragon")) == -1:
-    print('There is no dragon in this title')
-else:
-    print('dragon is on', movie.find("dragon"))
+import random
+d1 = random.randint(1,6)
+d2 = random.randint(1,6)
+d3 = random.randint(1,6)
+d4 = random.randint(1,6)
+print(d1, d2, d3, d4)
+print()
+print('Dice rolled: 4')
+print(f'Special number (1 or 6): {d1 == 1 or d1 == 6 or d2 == 1 or d2 == 6 or d3 == 1 or d3 == 6 or d4 == 1 or d4 == 6}')
