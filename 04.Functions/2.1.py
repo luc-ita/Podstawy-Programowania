9f388f0c1576cb98a3c2dfa85c50b903bf9ec7f0
luc-ita <lucaz5@vp.pl>
2024-12-14 01:31:55 +0100

    2.1

---
+++ 04.Functions/2.1.py
@@ -0,0 +1,9 @@
+print('Maximum to', max(7,5,6,3,8,2))
+print('Minimum to', min(4,7,2,3,9,8))
+print('lenght to' , len('computer science'))
+
+
+print('bin to', bin(304))
+print('hex to ', hex(304))
+print('euro', ord('€'))
+print('abs to', abs(-17))
